447090d8dadf5eaf903c79b03643f884d9133802
SILVERSTAR <silverstar.apollo@gmail.com>
2026-04-17 20:03:46 +0800

    size

--- include/slv/engine/math/size.hh
+++ include/slv/engine/math/size.hh
@@ -5,181 +5,173 @@ namespace slv {
 
 template<numeric T>
 struct size final {
-    constexpr size() = default;
-    constexpr explicit size(T width_val, T height_val) :
-        width(width_val),
-        height(height_val)
-    {}
-
     T width{};
     T height{};
 
-    [[nodiscard]] constexpr size<T> operator+(size<T> const& rhs) const {
-        return size<T>(
-            width + rhs.width,
-            height + rhs.height
-        );
+    [[nodiscard]] constexpr size<T> operator +(size<T> const& rhs) const {
+        return {
+            .width = width + rhs.width,
+            .height = height + rhs.height
+        };
     }
 
-    [[nodiscard]] constexpr size<T> operator-(size<T> const& rhs) const {
-        return size<T>(
-            width - rhs.width,
-            height - rhs.height
-        );
+    [[nodiscard]] constexpr size<T> operator -(size<T> const& rhs) const {
+        return {
+            .width = width - rhs.width,
+            .height = height - rhs.height
+        };
     }
 
-    [[nodiscard]] constexpr size<T> operator-() const {
-        return size<T>(
-            -width,
-            -height
-        );
+    [[nodiscard]] constexpr size<T> operator -() const {
+        return {
+            .width = -width,
+            .height = -height
+        };
     }
 
-    [[nodiscard]] constexpr size<T> operator*(size<T> const& rhs) const {
-        return size<T>(
-            width * rhs.width,
-            height * rhs.height
-        );
+    [[nodiscard]] constexpr size<T> operator *(size<T> const& rhs) const {
+        return {
+            .width = width * rhs.width,
+            .height = height * rhs.height
+        };
     }
 
-    [[nodiscard]] constexpr size<T> operator/(size<T> const& rhs) const {
-        return size<T>(
-            width / rhs.width,
-            height / rhs.height
-        );
-    }
-
-    template<numeric U>
-    [[nodiscard]] constexpr size<T> operator+(U rhs) const {
-        return size<T>(
-            width + rhs,
-            height + rhs
-        );
+    [[nodiscard]] constexpr size<T> operator /(size<T> const& rhs) const {
+        return {
+            .width = width / rhs.width,
+            .height = height / rhs.height
+        };
     }
 
     template<numeric U>
-    [[nodiscard]] constexpr size<T> operator-(U rhs) const {
-        return size<T>(
-            width - rhs,
-            height - rhs
-        );
+    [[nodiscard]] constexpr size<T> operator +(U rhs) const {
+        return {
+            .width = width + rhs,
+            .height = height + rhs
+        };
     }
 
     template<numeric U>
-    [[nodiscard]] constexpr size<T> operator*(U rhs) const {
-        return size<T>(
-            width * rhs,
-            height * rhs
-        );
+    [[nodiscard]] constexpr size<T> operator -(U rhs) const {
+        return {
+            .width = width - rhs,
+            .height = height - rhs
+        };
     }
 
     template<numeric U>
-    [[nodiscard]] constexpr size<T> operator/(U rhs) const {
-        return size<T>(
-            width / rhs,
-            height / rhs
-        );
+    [[nodiscard]] constexpr size<T> operator *(U rhs) const {
+        return {
+            .width = width * rhs,
+            .height = height * rhs
+        };
     }
 
-    constexpr size<T>& operator+=(size<T> const& rhs) {
+    template<numeric U>
+    [[nodiscard]] constexpr size<T> operator /(U rhs) const {
+        return {
+            .width = width / rhs,
+            .height = height / rhs
+        };
+    }
+
+    constexpr size<T>& operator +=(size<T> const& rhs) {
         width += rhs.width;
         height += rhs.height;
         return *this;
     }
 
     template<numeric U>
-    constexpr size<T>& operator+=(U rhs) {
+    constexpr size<T>& operator +=(U rhs) {
         width += rhs;
         height += rhs;
         return *this;
     }
 
-    constexpr size<T>& operator-=(size<T> const& rhs) {
+    constexpr size<T>& operator -=(size<T> const& rhs) {
         width -= rhs.width;
         height -= rhs.height;
         return *this;
     }
 
     template<numeric U>
-    constexpr size<T>& operator-=(U rhs) {
+    constexpr size<T>& operator -=(U rhs) {
         width -= rhs;
         height -= rhs;
         return *this;
     }
 
-    constexpr size<T>& operator*=(size<T> const& rhs) {
+    constexpr size<T>& operator *=(size<T> const& rhs) {
         width *= rhs.width;
         height *= rhs.height;
         return *this;
     }
 
     template<numeric U>
-    constexpr size<T>& operator*=(U rhs) {
+    constexpr size<T>& operator *=(U rhs) {
         width *= rhs;
         height *= rhs;
         return *this;
     }
 
-    constexpr size<T>& operator/=(size<T> const& rhs) {
+    constexpr size<T>& operator /=(size<T> const& rhs) {
         width /= rhs.width;
         height /= rhs.height;
         return *this;
     }
 
     template<numeric U>
-    constexpr size<T>& operator/=(U rhs) {
+    constexpr size<T>& operator /=(U rhs) {
         width /= rhs;
         height /= rhs;
         return *this;
     }
 
-    constexpr size<T>& operator++() {
+    constexpr size<T>& operator ++() {
         ++width;
         ++height;
         return *this;
     }
 
-    template<numeric U>
-    constexpr size<T> operator++(int) {
+    constexpr size<T> operator ++(int) {
         size<T> temp = *this;
         ++(*this);
         return temp;
     }
 
-    constexpr size<T>& operator--() {
+    constexpr size<T>& operator --() {
         --width;
         --height;
         return *this;
     }
 
-    template<numeric U>
-    constexpr size<T> operator--(int) {
+    constexpr size<T> operator --(int) {
         size<T> temp = *this;
         --(*this);
         return temp;
     }
 
-    constexpr bool operator==(size<T> const& rhs) const {
+    constexpr bool operator ==(size<T> const& rhs) const {
         return width == rhs.width && height == rhs.height;
     }
 
-    constexpr bool operator!=(size<T> const& rhs) const {
+    constexpr bool operator !=(size<T> const& rhs) const {
         return !(*this == rhs);
     }
 
-    constexpr bool operator<=(size<T> const& rhs) const {
+    constexpr bool operator <=(size<T> const& rhs) const {
         return width <= rhs.width && height <= rhs.height;
     }
 
-    constexpr bool operator>=(size<T> const& rhs) const {
+    constexpr bool operator >=(size<T> const& rhs) const {
         return width >= rhs.width && height >= rhs.height;
     }
 
-    constexpr bool operator<(size<T> const& rhs) const {
+    constexpr bool operator <(size<T> const& rhs) const {
         return width < rhs.width && height < rhs.height;
     }
 
-    constexpr bool operator>(size<T> const& rhs) const {
+    constexpr bool operator >(size<T> const& rhs) const {
         return width > rhs.width && height > rhs.height;
     }
 };
