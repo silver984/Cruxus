6515db10ce274291c4f12369d1f69b3d915df361
SILVERSTAR <silverstar.apollo@gmail.com>
2026-04-17 19:09:28 +0800

    vec2

--- include/slv/engine/math/vec2.hh
+++ include/slv/engine/math/vec2.hh
@@ -4,163 +4,161 @@
 namespace slv {
 
 template<numeric T>
-struct vec2 {
+struct vec2 final {
     T x{};
     T y{};
 
-    [[nodiscard]] constexpr vec2<T> operator+(vec2<T> const& rhs) const {
+    [[nodiscard]] constexpr vec2<T> operator +(vec2<T> const& rhs) const {
         return {
             .x = x + rhs.x,
             .y = y + rhs.y
         };
     }
 
-    [[nodiscard]] constexpr vec2<T> operator-(vec2<T> const& rhs) const {
-        return vec2<T>(
-            x - rhs.x,
-            y - rhs.y
-        );
+    [[nodiscard]] constexpr vec2<T> operator -(vec2<T> const& rhs) const {
+        return {
+            .x = x - rhs.x,
+            .y = y - rhs.y
+        };
     }
 
-    [[nodiscard]] constexpr vec2<T> operator-() const {
-        return vec2<T>(
-            -x,
-            -y
-        );
+    [[nodiscard]] constexpr vec2<T> operator -() const {
+        return {
+            .x = -x,
+            .y = -y
+        };
     }
 
-    [[nodiscard]] constexpr vec2<T> operator*(vec2<T> const& rhs) const {
-        return vec2<T>(
-            x * rhs.x,
-            y * rhs.y
-        );
+    [[nodiscard]] constexpr vec2<T> operator *(vec2<T> const& rhs) const {
+        return {
+            .x = x * rhs.x,
+            .y = y * rhs.y
+        };
     }
 
-    [[nodiscard]] constexpr vec2<T> operator/(vec2<T> const& rhs) const {
-        return vec2<T>(
-            x / rhs.x,
-            y / rhs.y
-        );
-    }
-
-    template<numeric U>
-    [[nodiscard]] constexpr vec2<T> operator+(U rhs) const {
-        return vec2<T>(
-            x + rhs,
-            y + rhs
-        );
+    [[nodiscard]] constexpr vec2<T> operator /(vec2<T> const& rhs) const {
+        return {
+            .x = x / rhs.x,
+            .y = y / rhs.y
+        };
     }
 
     template<numeric U>
-    [[nodiscard]] constexpr vec2<T> operator-(U rhs) const {
-        return vec2<T>(
-            x - rhs,
-            y - rhs
-        );
+    [[nodiscard]] constexpr vec2<T> operator +(U rhs) const {
+        return {
+            .x = x + rhs,
+            .y = y + rhs
+        };
     }
 
     template<numeric U>
-    [[nodiscard]] constexpr vec2<T> operator*(U rhs) const {
-        return vec2<T>(
-            x * rhs,
-            y * rhs
-        );
+    [[nodiscard]] constexpr vec2<T> operator -(U rhs) const {
+        return {
+            .x = x - rhs,
+            .y = y - rhs
+        };
     }
 
     template<numeric U>
-    [[nodiscard]] constexpr vec2<T> operator/(U rhs) const {
-        return vec2<T>(
-            x / rhs,
-            y / rhs
-        );
+    [[nodiscard]] constexpr vec2<T> operator *(U rhs) const {
+        return {
+            .x = x * rhs,
+            .y = y * rhs
+        };
     }
 
-    constexpr vec2<T>& operator+=(vec2<T> const& rhs) {
+    template<numeric U>
+    [[nodiscard]] constexpr vec2<T> operator /(U rhs) const {
+        return {
+            .x = x / rhs,
+            .y = y / rhs
+        };
+    }
+
+    constexpr vec2<T>& operator +=(vec2<T> const& rhs) {
         x += rhs.x;
         y += rhs.y;
         return *this;
     }
 
     template<numeric U>
-    constexpr vec2<T>& operator+=(U rhs) {
+    constexpr vec2<T>& operator +=(U rhs) {
         x += rhs;
         y += rhs;
         return *this;
     }
 
-    constexpr vec2<T>& operator-=(vec2<T> const& rhs) {
+    constexpr vec2<T>& operator -=(vec2<T> const& rhs) {
         x -= rhs.x;
         y -= rhs.y;
         return *this;
     }
 
     template<numeric U>
-    constexpr vec2<T>& operator-=(U rhs) {
+    constexpr vec2<T>& operator -=(U rhs) {
         x -= rhs;
         y -= rhs;
         return *this;
     }
 
-    constexpr vec2<T>& operator*=(vec2<T> const& rhs) {
+    constexpr vec2<T>& operator *=(vec2<T> const& rhs) {
         x *= rhs.x;
         y *= rhs.y;
         return *this;
     }
 
     template<numeric U>
-    constexpr vec2<T>& operator/=(U rhs) {
+    constexpr vec2<T>& operator /=(U rhs) {
         x /= rhs;
         y /= rhs;
         return *this;
     }
 
-    constexpr vec2<T>& operator++() {
+    constexpr vec2<T>& operator ++() {
         ++x;
         ++y;
         return *this;
     }
 
-    template<numeric U>
-    constexpr vec2<T> operator++(int) {
+    constexpr vec2<T> operator ++(int) {
         vec2<T> temp = *this;
         ++(*this);
         return temp;
     }
 
-    constexpr vec2<T>& operator--() {
+    constexpr vec2<T>& operator --() {
         --x;
         --y;
         return *this;
     }
 
-    template<numeric U>
-    constexpr vec2<T> operator--(int) {
+    constexpr vec2<T> operator --(int) {
         vec2<T> temp = *this;
         --(*this);
         return temp;
     }
 
-    constexpr bool operator==(vec2<T> const& rhs) const {
+    constexpr bool operator ==(vec2<T> const& rhs) const {
         return x == rhs.x && y == rhs.y;
     }
 
-    constexpr bool operator!=(vec2<T> const& rhs) const {
+    constexpr bool operator !=(vec2<T> const& rhs) const {
         return !(*this == rhs);
     }
 
-    constexpr bool operator<=(vec2<T> const& rhs) const {
+    constexpr bool operator <=(vec2<T> const& rhs) const {
         return x <= rhs.x && y <= rhs.y;
     }
 
-    constexpr bool operator>=(vec2<T> const& rhs) const {
+    constexpr bool operator >=(vec2<T> const& rhs) const {
         return x >= rhs.x && y >= rhs.y;
     }
 
-    constexpr bool operator<(vec2<T> const& rhs) const {
+    constexpr bool operator <(vec2<T> const& rhs) const {
         return x < rhs.x && y < rhs.y;
     }
 
-    constexpr bool operator>(vec2<T> const& rhs) const {
+    constexpr bool operator >(vec2<T> const& rhs) const {
         return x > rhs.x && y > rhs.y;
     }
 };
